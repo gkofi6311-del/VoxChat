a9a2ac49e5c0634347c70dc641408371d500510b
gkofi6311-del <gkofi6311@gmail.com>
2026-04-22 00:11:16 +0200

    Clean up VoxChatClient by removing unused code
    Removed commented-out code for keybinds and tick check.

--- voxchat/src/client/java/com/francis/voxchat/VoxChatClient.java
+++ voxchat/src/client/java/com/francis/voxchat/VoxChatClient.java
@@ -6,18 +6,6 @@ public class VoxChatClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
-        // register keybinds
         KeyBindings.register();
-
-        // example tick check (safe usage pattern)
-        net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.END_CLIENT_TICK.register(client -> {
-
-            if (KeyBindings.VOICE_RECORD != null && KeyBindings.VOICE_RECORD.wasPressed()) {
-                System.out.println("VOICE RECORD PRESSED");
-                // TODO: your voice logic here
-            }
-
-        });
     }
 }
