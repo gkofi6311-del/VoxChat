837b46a2295bb82791e007b3586fe7b74bdbf117
gkofi6311-del <gkofi6311@gmail.com>
2026-04-21 23:42:13 +0200

    Refactor KeyBindings to KeybindHandler and update keys

--- voxchat/src/client/java/com/francis/voxchat/KeyBindings.java
+++ voxchat/src/client/java/com/francis/voxchat/KeyBindings.java
@@ -1,22 +1,22 @@
-package com.francis.voxchat;
+package com.francis.voxchat.keybind;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-public final class KeyBindings {
+public class KeybindHandler {
 
-    private KeyBindings() {}
-
-    public static KeyBinding VOICE_RECORD;
+    public static KeyBinding TOGGLE_VOICE;
 
     public static void register() {
-        VOICE_RECORD = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.voxchat.voice_record",   // translation key
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_V,              // default: V
-                "category.voxchat"            // category in Controls screen
-        ));
+        TOGGLE_VOICE = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.voxchat.toggle",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_V,
+                        "category.voxchat"
+                )
+        );
     }
 }
