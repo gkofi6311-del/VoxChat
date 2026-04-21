7ff2c7b9284a64c7755b86fbfe3c4d9285eae37e
gkofi6311-del <gkofi6311@gmail.com>
2026-04-22 01:01:55 +0200

    Rename VOICE_KEY to PUSH_TO_TALK

--- voxchat/src/client/java/com/francis/voxchat/KeyBindings.java
+++ voxchat/src/client/java/com/francis/voxchat/KeyBindings.java
@@ -2,18 +2,16 @@ package com.francis.voxchat;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
 
-    public static KeyBinding VOICE_KEY;
+    public static KeyBinding PUSH_TO_TALK;
 
     public static void register() {
-        VOICE_KEY = KeyBindingHelper.registerKeyBinding(
+        PUSH_TO_TALK = KeyBindingHelper.registerKeyBinding(
             new KeyBinding(
                 "key.voxchat.push_to_talk",
-                InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_V,
                 "category.voxchat"
             )
